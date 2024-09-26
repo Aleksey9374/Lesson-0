bdb0ee205160260805e4fcccb17a4304b6ca7b68
Aleksey9374 <noyabrskhome75028@gmail.com>
2024-09-26 09:32:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Hello world")
+print('1st program')
+print(9 ** (1/2) * 5)
+print(9 ** 0.5 * 5)
+print('2nd program')
+print(9.99 > 9.98 and 1000 != 1000.1)
+print('3rd program')
+print(2 * 2 + 2 == 2 * (2 + 2))
+print('4th program')
+print(int(float('123.456') * 10 % 10))
